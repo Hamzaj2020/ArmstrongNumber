06988e956bf6a7c63ff96fcfe896f2614423f250
Hamzaj2020 <59518575+Hamzaj2020@users.noreply.github.com>
2020-02-05 13:26:49 +0000

    Add files via upload

---
+++ ArmstrongNumber1/src/main/java/org/example/ArmStrongNumber.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class ArmStrongNumber {
+
+    public static boolean isArmstrongNumber(Integer input) {
+
+        String str = input.toString();
+
+        int cubedNumber = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            cubedNumber += Math.pow(Integer.parseInt(String.valueOf(str.charAt(i))), 3);
+
+        }
+
+        System.out.println(cubedNumber);
+
+        return (cubedNumber == input);
+
+    }
+
+}
+
+
+
---
+++ ArmstrongNumber1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArmStrongNumber armstrongNumber = new ArmStrongNumber();
+
+        System.out.println(ArmStrongNumber.isArmstrongNumber(370));
+
+    }
+
+}
+
